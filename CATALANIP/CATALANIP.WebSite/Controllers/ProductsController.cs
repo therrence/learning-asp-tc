3fc651a9e65b2969ac70264e278d79fe830f3d98
therrence <58358238+therrence@users.noreply.github.com>
2019-12-03 16:38:54 +0800

    Initial Commit

---
+++ CATALANIP/CATALANIP.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CATALANIP.WebSite.Models;
+using CATALANIP.WebSite.Services;
+
+namespace CATALANIP.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+
+        }
+        [HttpGet]
+        [Route("Rate")]
+        public ActionResult Get(
+            [FromQuery]string ProductId,
+            [FromQuery]int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
+    }
+}
---
+++ CATALANIP/CATALANIP.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using CATALANIP.WebSite.Services;
+using CATALANIP.WebSite.Models;
+
+namespace CATALANIP.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+
+        public IndexModel
+            (ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
